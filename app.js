2ce75a1fbcaccc9166d6c3193e9e7db628868e56
Rodrigo <rvidela@outlook.com>
2019-06-14 19:59:57 -0300

    I add a get routes for several endpoints, it exceeds what is asked for in the trello card.

--- app.js
+++ app.js
@@ -64,10 +64,10 @@ app.get('/', (req, res, next) => {
   // console.log(req);
   // `Welcome, the requested data will be printed next: ${
   // res.json(gbody);
-  res.send('/');
+  res.send('Do you want to see albums, photos? Specify that in the endpoint!');
 });
 
-// `app.get()` call:
+//We show all the albums:
 app.get('/albums', (req, res) => {
   console.log('----/albums----');
 
@@ -82,21 +82,96 @@ app.get('/albums', (req, res) => {
   });
 });
 
-// `app.get()` call:
+// We show the album specified by its Id:
 app.get('/albums/:id', (req, res) => {
-  console.log('----/albums----');
-  console.log(req.params.id);
+  let id = req.params.id;
 
-  // request(`${url}/albums`, (error, response, body) => {
-  //   // Print the error if one occurred
-  //   console.log('error:', error);
+  console.log('\n\n----/albums by id number----');
+  console.log(`Album Id: ${id}`);
 
-  //   // Print the response status code if a response was received
-  //   console.log('statusCode:', response && response.statusCode);
+  request(`${url}/albums/${id}`, (error, response, body) => {
+    // Print the error if one occurred
+    console.log('error:', error);
 
-  //   res.send(body);
-  // });
+    // Print the response status code if a response was received
+    console.log('statusCode:', response && response.statusCode);
+
+    res.send(body);
+  });
 });
+
+// We show all the photos:
+app.get('/photos', (req, res) => {
+  console.log('----/photos----');
+  
+  request(`${url}/photos`, (error, response, body) => {
+    // Print the error if one occurred
+    console.log('error:', error);
+
+    // Print the response status code if a response was received
+    console.log('statusCode:', response && response.statusCode);
+
+    res.send(body);
+  });
+});
+
+// We show a photo by its Id:
+app.get('/photos/:id', (req, res) => {
+  let id = req.params.id;
+
+  console.log('----/photos by id number----');
+  console.log(`Photo Id: ${id}`);
+
+  request(`${url}/photos/${id}`, (error, response, body) => {
+    // Print the error if one occurred
+    console.log('error:', error);
+
+    // Print the response status code if a response was received
+    console.log('statusCode:', response && response.statusCode);
+
+    res.send(body);
+  });
+});
+
+// We show the photos from an album specified by its Id:
+app.get('/albums/:id/photos/', (req, res) => {
+  let id = req.params.id;
+
+  console.log('----/photos from a certain album(specified by its Id)----');
+  console.log(`Album Id: ${id}`);
+
+  request(`${url}/photos?albumId=${id}`, (error, response, body) => {
+    // Print the error if one occurred
+    console.log('error:', error);
+
+    // Print the response status code if a response was received
+    console.log('statusCode:', response && response.statusCode);
+
+    res.send(body);
+  });
+});
+
+
+// We show a photo from an album specified by its Id:
+app.get('/albums/:idAlbum/photos/:idPhoto', (req, res) => {
+  let idAlbum = req.params.idAlbum;
+  let idPhoto = req.params.idPhoto;
+
+  console.log('----/Photo(specified by its Id) from a certain album(specified by its Id)----');
+  console.log(`Album Id: ${idAlbum}`);
+  console.log(`Photo Id: ${idPhoto}`);
+
+  request(`${url}/photos?albumId=${idAlbum}&id=${idPhoto}`, (error, response, body) => {
+    // Print the error if one occurred
+    console.log('error:', error);
+
+    // Print the response status code if a response was received
+    console.log('statusCode:', response && response.statusCode);
+
+    res.send(body);
+  });
+});
+
 
 // Opens the port
 app.listen(PORT, () => {
