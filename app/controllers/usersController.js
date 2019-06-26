5804afc21ef74841b635dd96432778d855ac2bc6
Rodrigo <rvidela@outlook.com>
2019-06-26 19:01:34 -0300

    Now the user can be added and all the restrictions required by the card work

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -1,17 +1,40 @@
 const { registerUser } = require('../services/users');
 
 const addUser = (req, res) => {
-  const { body } = req;
+  const regexAlphanumeric = /^[a-zA-Z0-9]*$/;
+  // eslint-disable-next-line no-useless-escape
+  const regexValidEmail = /^([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)@wolox.com.ar$/;
 
-  console.log(body.firstName);
-  console.log(body.lastName);
-  console.log(body.email);
-  console.log(body.password);
+  const { firstName, lastName, email } = req.body;
+  let { password } = req.body;
 
-  registerUser(body)
-    .then(result => res.send(result.body))
+  const bcrypt = require('bcryptjs');
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(password, salt);
+
+  if (password.length < 8) {
+    res.status(500).send({ error: 'Password too short!' });
+    return;
+  }
+
+  if (regexAlphanumeric.test(password) === false) {
+    res.status(500).send({ error: 'Password is not alphanumeric!' });
+    return;
+  }
+
+  password = hash;
+
+  if (regexValidEmail.test(email) === false) {
+    res.status(500).send({ error: 'Email not valid!' });
+    return;
+  }
+
+  registerUser({ firstName, lastName, email, password })
+    .then(result => res.send(`The user ${firstName} ${lastName} was successfully created ${result.mybody}`))
     .catch(error =>
-      res.status(500).send({ error: `There were errors when adding the user: ${JSON.stringify(error)}` })
+      res.status(500).send({
+        error: `There were errors when adding the user ${firstName} ${lastName}: ${JSON.stringify(error)}`
+      })
     );
 };
 
